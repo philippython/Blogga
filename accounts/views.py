48488b8df40502cbe4eaaa4a1de3a99378c9921b
odulaja philip <odulajaphilip@gmail.com>
2022-09-28 15:31:42 +0100

    added signup template

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.contrib.auth.models import User
 
 # Create your views here.
+class SignUpView(CreateView):
+    model = User
+    fields = '__all__'
+    template_name = 'accounts/user_create_form.html'
