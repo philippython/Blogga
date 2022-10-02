d6232c8d853ce6d85001a22e19e5349819c4148e
odulaja philip <odulajaphilip@gmail.com>
2022-10-02 10:23:29 +0100

    added confirm email template

--- accounts/views.py
+++ accounts/views.py
@@ -20,7 +20,7 @@ class SignUpView(CreateView):
     def form_valid(self, form):
         
         html_content = 'accounts/verify.html'
-        context_data = {'name' : form.cleaned_data['first_name'], 'id' : form.cleaned_data.get('id')}
+        context_data = {'name' : form.cleaned_data['first_name'], 'username' : form.cleaned_data.get('username')}
         email_html_template =get_template(html_content).render(context_data)
         msg = EmailMessage('Welcome to Blogga', email_html_template, settings.EMAIL_HOST_USER, [form.cleaned_data['email']])
         msg.content_subtype = 'html'
@@ -45,7 +45,7 @@ def verify_email(request):
 def confirm_email(request):
     
     if request.method == 'GET':
-        user = get_object_or_404(User, pk=request.id)
+        user = get_object_or_404(User, username=request.username)
         user.is_active = True
         user.save()
         return redirect('login')
